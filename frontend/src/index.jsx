d0989fcc5a9dbd16a5e98a9c547a878479fba996
tsamantanis <philip.tsamantanis@icloud.com>
2021-05-06 16:09:51 -0700

    Setup apollo client

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
+import {
+    ApolloProvider,
+    ApolloClient,
+    createHttpLink,
+    InMemoryCache,
+} from '@apollo/client';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:4000',
+});
+
+const client = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-    <React.StrictMode>
+    <ApolloProvider client={client}>
         <App />
-    </React.StrictMode>,
+    </ApolloProvider>,
     document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.unregister();
