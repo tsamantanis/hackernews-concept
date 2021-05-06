692db4a70535709e7bc3ad1c055441ccdb48cd21
tsamantanis <philip.tsamantanis@icloud.com>
2021-05-06 15:36:36 -0700

    Add pagination capability for feed query

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -10,6 +10,8 @@ async function feed(parent, args, context) {
 
     const links = await context.prisma.link.findMany({
         where,
+        skip: args.skip,
+        take: args.take,
     });
 
     return links;
