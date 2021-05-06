78c4934f183c25bc316a1c4d7736f38cc5279038
tsamantanis <philip.tsamantanis@icloud.com>
2021-05-06 16:17:55 -0700

    Display dummy data in frontend

---
+++ frontend/src/components/Link.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+
+const Link = (props) => {
+    const { link } = props;
+    return (
+        <div>
+            <div>
+                {link.description}
+                {' '}
+                (
+                {link.url}
+                )
+            </div>
+        </div>
+    );
+};
+
+export default Link;
