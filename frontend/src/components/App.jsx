a6c672a916e7743c72a9d2914a2f0c00500f606c
tsamantanis <philip.tsamantanis@icloud.com>
2021-05-06 17:03:34 -0700

    Add navigation

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import LinkList from './LinkList';
+import { Switch, Route } from 'react-router-dom';
 import CreateLink from './CreateLink';
+import Header from './Header';
+import LinkList from './LinkList';
 
 function App() {
     return (
-        <>
-            <CreateLink />
-            <LinkList />
-        </>
+        <div className="center w85">
+            <Header />
+            <div className="ph3 pv1 background-gray">
+                <Switch>
+                    <Route exact path="/" component={LinkList} />
+                    <Route
+                        exact
+                        path="/create"
+                        component={CreateLink}
+                    />
+                </Switch>
+            </div>
+        </div>
     );
 }
 
