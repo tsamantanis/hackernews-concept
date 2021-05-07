70c0d005efbb29a6d44d265f13b86c17cb1e2857
tsamantanis <philip.tsamantanis@icloud.com>
2021-05-06 17:41:00 -0700

    setup graphql subscriptions with apollo

--- frontend/src/components/LinkList.jsx
+++ frontend/src/components/LinkList.jsx
@@ -27,8 +27,108 @@ export const FEED_QUERY = gql`
     }
 `;
 
+const NEW_LINKS_SUBSCRIPTION = gql`
+    subscription {
+        newLink {
+            id
+            url
+            description
+            createdAt
+            postedBy {
+                id
+                name
+            }
+            votes {
+                id
+                user {
+                id
+                }
+            }
+        }
+    }
+`;
+
+const NEW_VOTES_SUBSCRIPTION = gql`
+    subscription {
+        newVote {
+            id
+            link {
+                id
+                url
+                description
+                createdAt
+                postedBy {
+                    id
+                    name
+                }
+                votes {
+                    id
+                    user {
+                        id
+                    }
+                }
+            }
+            user {
+                id
+            }
+        }
+    }
+`;
+
+const getQueryVariables = (isNewPage, page) => {
+    const skip = isNewPage ? (page - 1) * LINKS_PER_PAGE : 0;
+    const take = isNewPage ? LINKS_PER_PAGE : 100;
+    const orderBy = { createdAt: 'desc' };
+    return { take, skip, orderBy };
+};
+
 const LinkList = () => {
-    const { data } = useQuery(FEED_QUERY);
+    const history = useHistory();
+    const isNewPage = history.location.pathname.includes(
+        'new',
+    );
+    const pageIndexParams = history.location.pathname.split(
+        '/',
+    );
+    const page = parseInt(
+        pageIndexParams[pageIndexParams.length - 1],
+    );
+
+    const pageIndex = page ? (page - 1) * LINKS_PER_PAGE : 0;
+
+    const {
+        data,
+        loading,
+        error,
+        subscribeToMore,
+    } = useQuery(FEED_QUERY, {
+        variables: getQueryVariables(isNewPage, page),
+    });
+
+    subscribeToMore({
+        document: NEW_LINKS_SUBSCRIPTION,
+        updateQuery: (prev, { subscriptionData }) => {
+            if (!subscriptionData.data) return prev;
+            const { newLink } = subscriptionData.data;
+            const exists = prev.feed.links.find(
+                ({ id }) => id === newLink.id,
+            );
+            if (exists) return prev;
+
+            return {
+                ...prev,
+                feed: {
+                    links: [newLink, ...prev.feed.links],
+                    count: prev.feed.links.length + 1,
+                    __typename: prev.feed.__typename,
+                },
+            };
+        },
+    });
+
+    subscribeToMore({
+        document: NEW_VOTES_SUBSCRIPTION,
+    });
 
     return (
         <div>
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,8 +7,12 @@ import {
     ApolloClient,
     createHttpLink,
     InMemoryCache,
+    split,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
+
+import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 import { AUTH_TOKEN } from './constants';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
@@ -27,8 +31,30 @@ const authLink = setContext((_, { headers }) => {
     };
 });
 
+const wsLink = new WebSocketLink({
+    uri: 'ws://localhost:4000/graphql',
+    options: {
+        reconnect: true,
+        connectionParams: {
+            authToken: localStorage.getItem(AUTH_TOKEN),
+        },
+    },
+});
+
+const link = split(
+    ({ query }) => {
+        const { kind, operation } = getMainDefinition(query);
+        return (
+            kind === 'OperationDefinition'
+        && operation === 'subscription'
+        );
+    },
+    wsLink,
+    authLink.concat(httpLink),
+);
+
 const client = new ApolloClient({
-    link: authLink.concat(httpLink),
+    link,
     cache: new InMemoryCache(),
 });
 
